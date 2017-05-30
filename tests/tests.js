33b0767c44f69f6bc0a6d2f1f4f085ed963ca53d
Hugo Zapata <hugozap@gmail.com>
2017-05-29 23:36:56 -0500

    format

--- tests/tests.js
+++ tests/tests.js
@@ -1,45 +1,44 @@
-var ReactDOM = require('react-dom')
-var React = require('react')
-var {Samy,Proxy} = require('../src/index')
+var ReactDOM = require("react-dom")
+var React = require("react")
+var { Samy, Proxy } = require("../src/index")
 
-var Basic = ()=>{
-    return <Samy path="1.svg" scene={{}}/>
+var Basic = () => {
+    return <Samy path="1.svg" scene={{}} />
 }
 
 class Timer extends React.Component {
-    
-    constructor(props)
-    {
+    constructor(props) {
         super(props)
         this.state = {
-            fill:'#000'
+            fill: "#000"
         }
     }
 
-    componentDidMount(){
-        setInterval( () => { 
-            var fillValue = this.state.fill === '#000' ? '#eee' : '#000'      
-            console.log('fillValue',fillValue)
-            this.setState({fill:fillValue})
-        },1000)
+    componentDidMount() {
+        setInterval(() => {
+            var fillValue = this.state.fill === "#000" ? "#eee" : "#000"
+            console.log("fillValue", fillValue)
+            this.setState({ fill: fillValue })
+        }, 1000)
     }
 
     render() {
-        return <Samy path="1.svg" scene={this.state}>
-                <Proxy select={"polygon"} fill={this.state.fill}/>
+        return (
+            <Samy path="1.svg" scene={this.state}>
+                <Proxy select={"polygon"} fill={this.state.fill} />
             </Samy>
+        )
     }
 }
 
-var appnode = document.createElement('div')
-appnode.setAttribute('id','container')
+var appnode = document.createElement("div")
+appnode.setAttribute("id", "container")
 document.body.appendChild(appnode)
 
 ReactDOM.render(
-      <div>
-        <Basic/>
-        <Timer/>
+    <div>
+        <Basic />
+        <Timer />
     </div>,
-      document.body.querySelector('#container')
-);
-
+    document.body.querySelector("#container")
+)
