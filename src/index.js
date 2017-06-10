1e81564b3893350b5641ef31164838c11d5b96aa
Hugo Zapata <hugozap@gmail.com>
2017-06-10 00:20:19 -0500

    added svgProps

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ class Samy extends React.Component {
   static propTypes = {
     path: React.PropTypes.string.isRequired,
     ref: React.PropTypes.func,
+    svgProps: React.PropTypes.object
   }
 
   static childContextTypes = {
@@ -29,6 +30,26 @@ class Samy extends React.Component {
   onSVGReady (svgNode) {
     this.setState({svg: svgNode})
     this.props.ref(svgNode)
+
+    //set svgProps
+      if (svgNode && this.props.svgProps) {
+        var keys = Object.keys(this.props.svgProps)
+        keys.forEach((k)=>{
+          svgNode.setAttribute(k, this.props.svgProps[k])
+        })
+      }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    //Apply properties to svg element
+    if (this.props.svgProps  != nextProps.svgProps) {
+      if (this.state.svg) {
+        var keys = Object.keys(nextProps.svgProps)
+        keys.forEach((k)=>{
+          this.state.svg.setAttribute(k, nextProps.svgProps[k])
+        })
+      }
+    }
   }
 
   render () {
--- tests/tests.js
+++ tests/tests.js
@@ -10,7 +10,8 @@ class Timer extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            fill: "#000"
+            fill: "#000",
+            offsetX:0
         }
     }
 
@@ -18,17 +19,21 @@ class Timer extends React.Component {
         setInterval(() => {
             var fillValue = this.state.fill === "#000" ? "#eee" : "#000"
             console.log("fillValue", fillValue)
-            this.setState({ fill: fillValue })
+            this.setState({ fill: fillValue, offsetX: Math.random()*50 })
         }, 1000)
     }
 
     render() {
         return (
-            <Samy path="1.svg" scene={this.state}>
+            <Samy path="1.svg" svgProps={{viewBox:`${this.state.offsetX} 0 50 50`}}>
                 <Proxy select={"polygon"} fill={this.state.fill} />
             </Samy>
         )
     }
+}
+
+var ModifyViewBox = () => {
+    return <Samy path="1.svg" svgProps={{viewBox:'0 0 40 40'}}/>
 }
 
 var appnode = document.createElement("div")
@@ -37,8 +42,12 @@ document.body.appendChild(appnode)
 
 ReactDOM.render(
     <div>
+        Load an inject SVG
         <Basic />
+        Change svg properties (viewbox) and fillcolor
         <Timer />
+        Change viewbox
+        <ModifyViewBox/>
     </div>,
     document.body.querySelector("#container")
 )
