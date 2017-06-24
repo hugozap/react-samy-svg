133438f5a27718a878dcece911959bc590c3b5c0
Hugo Zapata <hugozap@gmail.com>
2017-06-24 13:58:49 -0500

    Move Samy to separate class

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,16 @@
 (function (global, factory) {
   if (typeof define === "function" && define.amd) {
-    define(['exports', 'react', './Proxy', './SVGLoader'], factory);
+    define(['exports', './Samy', './Proxy'], factory);
   } else if (typeof exports !== "undefined") {
-    factory(exports, require('react'), require('./Proxy'), require('./SVGLoader'));
+    factory(exports, require('./Samy'), require('./Proxy'));
   } else {
     var mod = {
       exports: {}
     };
-    factory(mod.exports, global.react, global.Proxy, global.SVGLoader);
+    factory(mod.exports, global.Samy, global.Proxy);
     global.index = mod.exports;
   }
-})(this, function (exports, _react, _Proxy, _SVGLoader) {
+})(this, function (exports, _Samy, _Proxy) {
   'use strict';
 
   Object.defineProperty(exports, "__esModule", {
@@ -18,11 +18,9 @@
   });
   exports.Samy = exports.Proxy = undefined;
 
-  var _react2 = _interopRequireDefault(_react);
+  var _Samy2 = _interopRequireDefault(_Samy);
 
   var _Proxy2 = _interopRequireDefault(_Proxy);
-
-  var _SVGLoader2 = _interopRequireDefault(_SVGLoader);
 
   function _interopRequireDefault(obj) {
     return obj && obj.__esModule ? obj : {
@@ -30,138 +28,6 @@
     };
   }
 
-  function _classCallCheck(instance, Constructor) {
-    if (!(instance instanceof Constructor)) {
-      throw new TypeError("Cannot call a class as a function");
-    }
-  }
-
-  function _possibleConstructorReturn(self, call) {
-    if (!self) {
-      throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
-    }
-
-    return call && (typeof call === "object" || typeof call === "function") ? call : self;
-  }
-
-  var _createClass = function () {
-    function defineProperties(target, props) {
-      for (var i = 0; i < props.length; i++) {
-        var descriptor = props[i];
-        descriptor.enumerable = descriptor.enumerable || false;
-        descriptor.configurable = true;
-        if ("value" in descriptor) descriptor.writable = true;
-        Object.defineProperty(target, descriptor.key, descriptor);
-      }
-    }
-
-    return function (Constructor, protoProps, staticProps) {
-      if (protoProps) defineProperties(Constructor.prototype, protoProps);
-      if (staticProps) defineProperties(Constructor, staticProps);
-      return Constructor;
-    };
-  }();
-
-  function _inherits(subClass, superClass) {
-    if (typeof superClass !== "function" && superClass !== null) {
-      throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);
-    }
-
-    subClass.prototype = Object.create(superClass && superClass.prototype, {
-      constructor: {
-        value: subClass,
-        enumerable: false,
-        writable: true,
-        configurable: true
-      }
-    });
-    if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
-  }
-
-  var Samy = function (_React$Component) {
-    _inherits(Samy, _React$Component);
-
-    _createClass(Samy, [{
-      key: 'getChildContext',
-      value: function getChildContext() {
-        return {
-          svg: this.state.svg
-        };
-      }
-    }]);
-
-    function Samy(props) {
-      _classCallCheck(this, Samy);
-
-      var _this = _possibleConstructorReturn(this, (Samy.__proto__ || Object.getPrototypeOf(Samy)).call(this, props));
-
-      _this.state = {
-        svg: null
-      };
-      return _this;
-    }
-
-    _createClass(Samy, [{
-      key: 'onSVGReady',
-      value: function onSVGReady(svgNode) {
-        var _this2 = this;
-
-        //set svgAttributes
-        if (svgNode && this.props.svgAttributes) {
-          var keys = Object.keys(this.props.svgAttributes);
-          keys.forEach(function (k) {
-            svgNode.setAttribute(k, _this2.props.svgAttributes[k]);
-          });
-        }
-
-        this.setState({ svg: svgNode });
-        this.props.onSVGReady(svgNode);
-      }
-    }, {
-      key: 'componentWillReceiveProps',
-      value: function componentWillReceiveProps(nextProps) {
-        var _this3 = this;
-
-        //Apply properties to svg element
-        if (this.props.svgAttributes != nextProps.svgAttributes) {
-          if (this.state.svg) {
-            var keys = Object.keys(nextProps.svgAttributes);
-            keys.forEach(function (k) {
-              _this3.state.svg.setAttribute(k, nextProps.svgAttributes[k]);
-            });
-          }
-        }
-      }
-    }, {
-      key: 'render',
-      value: function render() {
-        return _react2.default.createElement(
-          'div',
-          null,
-          ' ',
-          _react2.default.createElement(_SVGLoader2.default, { className: this.props.className || '', style: this.props.style, path: this.props.path, onSVGReady: this.onSVGReady.bind(this) }),
-          this.props.children,
-          ' '
-        );
-      }
-    }]);
-
-    return Samy;
-  }(_react2.default.Component);
-
-  Samy.propTypes = {
-    path: _react2.default.PropTypes.string.isRequired,
-    onSVGReady: _react2.default.PropTypes.func,
-    svgAttributes: _react2.default.PropTypes.object
-  };
-  Samy.childContextTypes = {
-    svg: _react2.default.PropTypes.object
-  };
-
-
-  Samy.defaultProps = {
-    onSVGReady: new Function()
-  };
   exports.Proxy = _Proxy2.default;
-  exports.Samy = Samy;
+  exports.Samy = _Samy2.default;
 });
---
+++ src/Samy.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import SVGLoader from './SVGLoader'
+
+class Samy extends React.Component {
+
+  static propTypes = {
+    path: React.PropTypes.string.isRequired,
+    onSVGReady: React.PropTypes.func,
+    svgAttributes: React.PropTypes.object
+  }
+
+  static childContextTypes = {
+    svg: React.PropTypes.object
+  }
+
+  getChildContext () {
+    return {
+      svg: this.state.svg
+    }
+  }
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      svg: null
+    }
+  }
+  onSVGReady (svgNode) {
+    //set svgAttributes
+    if (svgNode && this.props.svgAttributes) {
+      var keys = Object.keys(this.props.svgAttributes)
+      keys.forEach((k)=>{
+        svgNode.setAttribute(k, this.props.svgAttributes[k])
+      })
+    }
+
+    this.setState({svg: svgNode})
+    this.props.onSVGReady(svgNode)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    //Apply properties to svg element
+    if (this.props.svgAttributes  != nextProps.svgAttributes) {
+      if (this.state.svg) {
+        var keys = Object.keys(nextProps.svgAttributes)
+        keys.forEach((k)=>{
+          this.state.svg.setAttribute(k, nextProps.svgAttributes[k])
+        })
+      }
+    }
+  }
+
+  render () {
+    return <div> <SVGLoader className={this.props.className || ''} style={this.props.style} path={this.props.path} onSVGReady={this.onSVGReady.bind(this)} />
+      {this.props.children} </div>
+  }
+}
+
+Samy.defaultProps = {
+  onSVGReady: new Function()
+}
+
+export default Samy
